9b8eb9f63ec2a8368c31dc34b50ac02996a840f6
Surabhi Mali <61287447+surabhimali@users.noreply.github.com>
2024-10-12 19:34:30 +0530

    Update openai_app.py

--- openai_app.py
+++ openai_app.py
@@ -28,7 +28,7 @@ input_text=st.text_input("Search the topic you want")
 
 ## OpenAI LLM
 
-llm = ChatOpenAI(model="gpt-3.5-turbo")
+llm = ChatOpenAI(model="gpt-4o-mini")
 output_parser = StrOutputParser()
 chain = prompt|llm|output_parser
 
